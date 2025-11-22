bed0d0aaec512af656e21de97e0b2c26d1f4064a
Fabio Silveira <fabio.eds@hotmail.com>
2025-11-21 16:06:47 -0800

    updatedCode

--- src/App.jsx
+++ src/App.jsx
@@ -1,60 +1,62 @@
 import { useEffect, useState } from "react"
 
-export default function Light() {
-
+export default function App() {
+  
   const cycle = {
     red: "green",
     yellow: "red",
     green: "yellow"
   }
 
-  const [light, setLight] = useState("red")
+  const [light, setLight] = useState("red");
 
   useEffect(() => {
     const interval = setInterval(() => {
       setLight((prev) => cycle[prev])
-    }, 2000)
-
-    return () => clearInterval(interval)
+    }, 2000);
+    return() => clearInterval(interval)
   }, [])
-
+   
+  
   return (
     <>
-      <div>
-        <div style={{
-          height: "180px",
-          width: "80px",
-          backgroundColor: "black",
-          display: "inline-block",
-          borderRadius: "10px"
-        }}>
-          <div
-            style={{
-              height: "40px",
-              width: "40px",
-              backgroundColor: light === "red" ? "red" : "#555",
-              borderRadius: "50%",
-              margin: "10px auto"
-            }}
-          />
-          <div
-            style={{
-              height: "40px",
-              width: "40px",
-              backgroundColor: light === "yellow" ? "yellow" : "#555",
-              borderRadius: "50%",
-              margin: "10px auto"
-            }}
-          />
-          <div
-            style={{
-              height: "40px",
-              width: "40px",
-              backgroundColor: light === "green" ? "green" : "#555",
-              borderRadius: "50%",
-              margin: "10px auto"
-            }}
-          />
+      <div style={{
+        height: "800px",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+        <div
+          style={{
+            height: "200px",
+            width: "60px",
+            backgroundColor: "black",
+            borderRadius: "8px",
+            display: "inline-block",
+            padding: "22px"
+          }}
+        >
+          <div style={{
+            height: "60px",
+            width: "60px",
+            borderRadius: "50%",
+            backgroundColor: light === "red" ? "red" : "rgb(71, 66, 66)",
+            marginBottom: "10px"
+          }} />
+          <div style={{
+            height: "60px",
+            width: "60px",
+            borderRadius: "50%",
+            backgroundColor: light === "yellow" ? "yellow" : "rgb(71, 66, 66)",
+            marginBottom: "10px"
+          }} />
+          <div style={{
+            height: "60px",
+            width: "60px",
+            borderRadius: "50%",
+            backgroundColor: light === "green" ? "green" : "rgb(71, 66, 66)",
+          }} />
         </div>
       </div>
     </>
